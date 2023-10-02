9297b7ff1e2e9ddabfc70481d346b443fadf5535
krhernan0101 <71098762+krhernan0101@users.noreply.github.com>
2023-10-01 19:32:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main extends SplashJava{
+    public static void main(String[] args) {
+   
+   SplashJava splash = new SplashJava();
+    
+    //System.out.println(t.getID());
+
+}
+}
